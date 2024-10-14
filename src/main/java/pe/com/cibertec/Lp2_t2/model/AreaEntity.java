69b3f0833483ffba907cf6c60ade4b34c3df83bd
Jazmin <nikkobl@proton.me>
2024-10-13 23:18:31 -0500

    Actualiza application.properties

---
+++ src/main/java/pe/com/cibertec/Lp2_t2/model/AreaEntity.java
@@ -0,0 +1,36 @@
+package pe.com.cibertec.Lp2_t2.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_area")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AreaEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer area_id;
+    
+	@Column(name = "nombre_area", length = 45, nullable = false)
+    private String nombreArea;
+	
+	@OneToMany(mappedBy = "area", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<EmpleadoEntity> empleados;
+}
---
+++ src/main/java/pe/com/cibertec/Lp2_t2/model/EmpleadoEntity.java
@@ -0,0 +1,46 @@
+package pe.com.cibertec.Lp2_t2.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_empleado")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmpleadoEntity {
+
+	@Id
+	@Column(name = "dni_empleado", length = 8)
+	private String dniEmpleado;
+
+	@Column(name = "nombre_empleado", length = 45, nullable = false)
+	private String nombreEmpleado;
+
+	@Column(name = "apellido_empleado", length = 45, nullable = false)
+	private String apellidoEmpleado;
+
+	@Column(name = "fecha_nacimiento", nullable = false)
+	private Date fechaNacimiento;
+
+	@Column(name = "direccion", length = 45)
+	private String direccion;
+
+	@Column(name = "correo", length = 45)
+	private String correo;
+
+	@ManyToOne
+	@JoinColumn(name = "area_id", nullable = false)
+	private AreaEntity area;
+}
