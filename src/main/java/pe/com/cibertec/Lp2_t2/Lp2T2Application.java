f7c6f47c3f5b6630226b9b64f46b7466adf4107d
Jazmin <nikkobl@proton.me>
2024-10-13 21:15:45 -0500

    estructura inicial

---
+++ src/main/java/pe/com/cibertec/Lp2_t2/Lp2T2Application.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.Lp2_t2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2T2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2T2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/cibertec/Lp2_t2/Lp2T2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.Lp2_t2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2T2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
